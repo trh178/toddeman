5c57c9827f38738f7f16cfb3d5b90f586756768b
trh178 <trh178@gmail.com>
2011-02-23 20:09:59 -0500

    Initial commit of toddeman.com project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toddeman::Application.config.secret_token = '856df8213435f96d6b4dec68c47e0d8e9566b34fc7d6be320b2a938d464e28821c80cad2fa9569db540b0c0b7ab0ed4367e2ae06da9cf166744b52665d86a5ed'
